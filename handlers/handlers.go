28fe101a5ec97c6b3130a35a721f5e346339a7b7
Jishnu Sanyal <sanyaljishnu@gmail.com>
2019-12-23 19:57:31 +0530

    Finished POST Module

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -35,7 +35,19 @@ func ListWinners(res http.ResponseWriter, req *http.Request) {
 
 // AddNewWinner adds new winner to the list
 func AddNewWinner(res http.ResponseWriter, req *http.Request) {
+	accessToken := req.Header.Get("X-ACCESS-TOKEN")
+	isAccessTokenValid := data.IsAccessTokenValid(accessToken)
+	if !isAccessTokenValid {
+		res.WriteHeader(http.StatusUnauthorized)
+	} else {
+		err := data.AddNewWinner(req.Body)
+		if err != nil {
+			res.WriteHeader(http.StatusUnprocessableEntity)
+			return
+		}
+		res.WriteHeader(http.StatusCreated)
 
+	}
 }
 
 // WinnersHandler is the dispatcher for all /winners URL
